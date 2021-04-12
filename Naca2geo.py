dc6e30b0be647531a2d8a477e5e219bfe3bcd76c
eliesls <elielesec@gmail.com>
2021-04-12 12:09:16 +0200

    Create Naca2geo.py

---
+++ Naca2geo.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+NACA = '2415'
+
+file = open(f"Coords//naca{NACA}.dat","r")
+
+file2 = open("NACA_geo.txt","w")
+
+counter = 1
+
+file2.write('c1=0.01;\n')
+for line in file:
+    if counter!=1:
+        u = line.split(' ')
+        x = u[0]
+        y = u[len(u)-1][:-2]
+        z = 0
+        string = f"Point({counter}) = {{{x}, {y}, {z}, c1}};\n"
+        file2.write(string)
+        counter+=1
+
+counter-=1
+L = [counter] + [k for k in range(1,counter+1)]
+spline_string = f"Spline(1) = {{{str(L)[1:-1]}}};"
+file2.write(spline_string)
+
